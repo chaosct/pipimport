953b4f217d460462d3bbf8833416946234ea533c
Carles F. Julià <carles.fernandez@upf.edu>
2013-10-26 01:47:52 +0200

    Now it can import full module names like "zope.interface"

--- pipimport/__init__.py
+++ pipimport/__init__.py
@@ -4,9 +4,11 @@
 import sys
 import os.path
 import subprocess
-import importlib
+import site
+
 
 pip_bin = os.path.join(sys.prefix,'bin','pip')
+python_bin = os.path.join(sys.prefix,'bin','python')
 
 class PipInstallError(Exception):
 	pass
@@ -17,27 +19,29 @@ def pip_install(name):
 	except subprocess.CalledProcessError:
 		raise PipInstallError
 
-class PipLoader(object):
+def rescan_path():
+	paths = [s for s in sys.path if s.startswith(sys.prefix)
+		and (s.endswith('site-packages') or s.endswith('site-python'))]
+	site.addsitedir(paths[0])
+
+
+class ImportReplacement(object):
 	def __init__(self):
-		self.module = None
+		self.realimport = __import__
 
-	def find_module(self, name, path):
-		print "Will install module {}".format(name)
-		self.module = None
-		sys.meta_path.remove(self)
+	def __call__(self, name, *args, **kwargs):
 		try:
-			pip_install(name)
-			self.module = importlib.import_module(name)
-		finally:
-			sys.meta_path.append(self)
-		return self
+			return self.realimport(name, *args, **kwargs)
+		except ImportError:
+			pass
+		print "Will install module {}".format(name)
+		pip_install(name)
+		rescan_path()
+		return self.realimport(name, *args, **kwargs)
+		
 
-	def load_module(self, name):
-		if not self.module:
-			raise ImportError("Unable to load module")
-		module = self.module
-		sys.modules[name] = module
-		return module
 
 def install():
-	sys.meta_path.append(PipLoader())
+	import __builtin__
+	importreplacement = ImportReplacement()
+	__builtin__.__import__ = importreplacement
