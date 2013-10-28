c06cb7b3ae9a54c137db487e22db8f38d08c5539
Carles F. Julià <carles.fernandez@upf.edu>
2013-10-28 01:54:40 +0100

    remember modules that can't be installed

--- pipimport/__init__.py
+++ pipimport/__init__.py
@@ -5,10 +5,22 @@ import sys
 import os.path
 import subprocess
 import site
-
+import json
+import atexit
 
 pip_bin = os.path.join(sys.prefix,'bin','pip')
 python_bin = os.path.join(sys.prefix,'bin','python')
+ignore_list_f = [os.path.join(s,".pipimport-ignore.json")
+					for s in [sys.prefix,'.']]
+
+def openone(flist, *args):
+	for p in flist:
+		try:
+			f = open(p, *args)
+			return (p,f)
+		except IOError:
+			pass
+	return None,None
 
 class PipInstallError(Exception):
 	pass
@@ -28,19 +40,35 @@ def rescan_path():
 class ImportReplacement(object):
 	def __init__(self):
 		self.realimport = __import__
+		self.ignoref, f = openone(ignore_list_f)
+		if f:
+			self.ignore = set(json.load(f))
+			f.close()
+		else:
+			self.ignore = set()
 
 	def __call__(self, name, *args, **kwargs):
 		try:
 			return self.realimport(name, *args, **kwargs)
 		except ImportError:
 			pass
+		if name in self.ignore:
+			raise ImportError
 		print "Will install module {}".format(name)
 		try:
 			pip_install(name)
 		except PipInstallError:
+			self.ignore.add(name)
 			raise ImportError
 		rescan_path()
 		return self.realimport(name, *args, **kwargs)
+
+	def saveignore(self):
+		fl = [i for i in [self.ignoref]+ignore_list_f if i]
+		p, f = openone(fl, 'w')
+		if f:
+			json.dump(list(self.ignore),f)
+			f.close()
 		
 
 
@@ -50,3 +78,11 @@ def install():
 		return
 	importreplacement = ImportReplacement()
 	__builtin__.__import__ = importreplacement
+
+
+@atexit.register
+def uninstall():
+	import __builtin__
+	if isinstance(__builtin__.__import__, ImportReplacement):
+		__import__.saveignore()
+		__builtin__.__import__ = __import__.realimport
